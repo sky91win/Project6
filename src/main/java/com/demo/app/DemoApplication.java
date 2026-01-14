154ce048c05001a5f94ee8d7b3c4537188d88472
sky91win <118115504+sky91win@users.noreply.github.com>
2026-01-14 15:53:29 +0530

    Update DemoApplication.java

--- src/main/java/com/demo/app/DemoApplication.java
+++ src/main/java/com/demo/app/DemoApplication.java
@@ -1,12 +1,12 @@
-package com.demo.app;
+package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class JavaDemoAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(JavaDemoAppApplication.class, args);
     }
 }
